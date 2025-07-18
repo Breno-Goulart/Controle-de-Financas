b3a8f64f980bbe5d8de20298d43c97b608f9b5b7
Breno <breno_goulart98@outlook.com>
2025-07-17 22:30:36 -0300

    sim

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,20 @@
-// vite.config.js - VERSÃO FINAL PARA ESTRUTURA ORGANIZADA
-const { resolve } = require('path');
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-module.exports = {
-  // Define a 'src' como a pasta raiz do nosso código
-  root: 'src',
-
+export default defineConfig({
   build: {
-    // Define o diretório de saída para uma pasta 'dist' na raiz do projeto
-    outDir: '../dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        cadastro: resolve(__dirname, 'src/cadastro.html'),
-        configuracoes: resolve(__dirname, 'src/configuracoes.html'),
-        familia: resolve(__dirname, 'src/familia.html'),
-        historico: resolve(__dirname, 'src/historico.html'),
-        lancamentos: resolve(__dirname, 'src/lancamentos.html'),
         login: resolve(__dirname, 'src/login.html'),
+        cadastro: resolve(__dirname, 'src/cadastro.html'),
+        familia: resolve(__dirname, 'src/familia.html'),
+        lancamentos: resolve(__dirname, 'src/lancamentos.html'),
+        configuracoes: resolve(__dirname, 'src/configuracoes.html'),
+        historico: resolve(__dirname, 'src/historico.html'),
         relatorio: resolve(__dirname, 'src/relatorio.html'),
       },
     },
   },
-};
+});
