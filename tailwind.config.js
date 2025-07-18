d3dbc22851560c71ed0af4e2cd32d90d75f8e8c4
Breno <breno_goulart98@outlook.com>
2025-07-17 21:06:47 -0300

    mudei

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // A linha mais importante é esta: 'darkMode: "class"'
-  darkMode: 'class',
-
+  darkMode: 'class', // A linha mais importante!
   content: [
-    // Diz ao Tailwind para procurar classes em todos os ficheiros HTML e JS
     './src/**/*.{html,js}',
   ],
   theme: {
